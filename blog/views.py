8f18bb011990356637cf56dbbc01cf16588d9ca6
zhanghang <hz2447@nyu.edu>
2021-08-19 13:55:21 -0400

    add Pagination function in the homepage

--- blog/views.py
+++ blog/views.py
@@ -4,37 +4,44 @@ from django.http import HttpResponse
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
-    ListView, 
+    ListView,
     DetailView,
     CreateView,
     UpdateView,
     DeleteView)
 
-#function view
+# function view
+
+
 def home(request):
 
     context = {
         'posts': Post.objects.all(),
         'title': 'blog-home'
     }
-    return render(request, 'blog/home.html' ,context)
+    return render(request, 'blog/home.html', context)
 
-#class-based view, it is a list-type view
+# class-based view, it is a list-type view
+
+
 class PostListView(ListView):
     model = Post
-    template_name= 'blog/home.html' #the default is <app>/<model>_<viewtype>.html
+    template_name = 'blog/home.html'  # the default is <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
-    ordering = ['-date_posted'] # setup the order of sql query
+    ordering = ['-date_posted']  # setup the order of sql query
+    paginate_by = 5 # every page has 5 objects
+
 
 class PostDetailView(DetailView):
     model = Post
+
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
 
     def form_valid(self, form):
-        form.instance.author=self.request.user
+        form.instance.author = self.request.user
         return super().form_valid(form)
 
 
@@ -43,7 +50,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     fields = ['title', 'content']
 
     def form_valid(self, form):
-        form.instance.author=self.request.user
+        form.instance.author = self.request.user
         return super().form_valid(form)
 
     def test_func(self):
@@ -52,15 +59,17 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
             return True
         return False
 
+
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     success_url = '/'
+
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:
             return True
         return False
-    
-def about(request):
-    return render(request, 'blog/about.html' )
 
+
+def about(request):
+    return render(request, 'blog/about.html')
